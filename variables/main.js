e2af764675cc7bb7fb424c0e6560505c0884477a
Irina Butuzova <butuzovairina18@gmail.com>
2019-06-23 15:08:34 +0300

    First homework

---
+++ variables/main.js
@@ -0,0 +1,17 @@
+// 1
+let algebra = 5, geography = 4, physics = 4;
+let average = (algebra + geography + physics) / 3;
+console.log(`Средний балл = ${average}`);
+
+//2
+let name = 'Ирина';
+let message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
+
+//3
+let x, y, z;
+x = 2;
+y = 22;
+z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(`result = ${result}`);
