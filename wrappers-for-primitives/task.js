83510f427a7967c1a13269765b22a837f91b7ed3
Irina Butuzova <butuzovairina18@gmail.com>
2019-07-08 16:11:54 +0300

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,18 +1,53 @@
+"use strict";
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
     let amount = window.amount.value;
     let date = window.date.value;
-
     let result = calculateTotalMortgage(percent, contribution, amount, date);
     let span = window.mortageResult;
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function checkInput (stringInput, nameParametr) {
+  let value;
+  if (typeof stringInput === "string") {
+    return value = parseFloat(stringInput);
+  } else if (typeof stringInput === "number") {  
+    return value = stringInput;   
+  } else { 
+    return  console.log(`Параметр "${nameParametr}" содержит неправильное значение "${stringInput}".`);
+  }
+} 
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+
+function monthDiff(dateToDay, date) {
+  let months;
+  months = (date.getFullYear() - dateToDay.getFullYear()) * 12;
+  months -= dateToDay.getMonth() + 1;
+  months += date.getMonth();
+  return months <= 0 ? 0 : months;
+}
+
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+    // код для задачи №1 писать здесь 
+
+  percent = checkInput(percent, "Процентная ставка");
+  contribution = checkInput(contribution, "Начальный взнос");
+  amount = checkInput(amount, "Общая стоимость");
+  let sumDebt = amount - contribution;
+  //console.log(sumDebt);
+  let dateToDay = new Date();
+  let dateEnd = new Date(date);
+  let numberOfMonth = monthDiff(dateToDay, dateEnd);   
+  let percInMonth = percent / 12 / 100; 
+  let payInMonth = sumDebt * ( percInMonth + percInMonth / (Math.pow( 1 + percInMonth, numberOfMonth )-1 ));
+ // console.log(payInMonth);
+  let totalAmount = payInMonth * numberOfMonth;
+ // console.log(totalAmount);
+  return totalAmount;
 }
 
 function sayHello() {
@@ -24,5 +59,21 @@ function sayHello() {
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    //return greeting;
+  let greeting;
+  /*switch (name) {
+    case "":
+    case "null":
+    case "undefined":
+      greeting = `Привет, мир! Меня зовут Аноним.`;
+    default:
+      greeting = `Привет, мир! Меня зовут ${name}.`;
+  } не работает (*/
+
+  if (name === "" || name === "null" || name === "undefined") {
+    greeting = `Привет, мир! Меня зовут Аноним.`;
+  } else {
+    greeting = `Привет, мир! Меня зовут ${name}.`;
+  }
+  console.log(greeting);
+  return greeting;
 }
