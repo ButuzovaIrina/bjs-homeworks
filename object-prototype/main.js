0900cb2458529405f4fde7b319c6d415d3501b17
Irina Butuzova <butuzovairina18@gmail.com>
2019-07-08 19:20:58 +0300

    first commit object-prototype

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -8,6 +8,11 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
+  let now = Date.now();
+  birthday = Date.parse(birthday);
+  let diff = now - birthday;
+  let age = Math.round(diff/(365.242 * 24 * 60 * 60 *1000));
+  return (age >= 18) ? true :  false;
 }
 
 function initPrintAnimalSound() {
@@ -22,6 +27,12 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
+  if (animal === undefined) {
+    return null;
+  } else {
+    let sound = animal.sound;
+    return sound;
+  }
 }
 
 function initCalculateStatement() {
@@ -36,4 +47,11 @@ function initCalculateStatement() {
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
+  let average = 0;
+  let i;
+  for (i = 0; i < marks.length; i++ ) {
+    average = average + parseInt(marks[i]); 
+  }  
+  let roundedAverage = Math.round(average/i);
+  return roundedAverage;
 }
