1ef3cb9b60682085271b1fe1ea8a456808bbd531
Irina Butuzova <butuzovairina18@gmail.com>
2019-07-11 20:15:12 +0300

    second commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -34,19 +34,16 @@ function monthDiff(dateToDay, date) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь 
 
-  percent = checkInput(percent, "Процентная ставка");
-  contribution = checkInput(contribution, "Начальный взнос");
-  amount = checkInput(amount, "Общая стоимость");
-  let sumDebt = amount - contribution;
-  //console.log(sumDebt);
+  let percentInput = checkInput(percent, "Процентная ставка");
+  let contributionInput = checkInput(contribution, "Начальный взнос");
+  let amountInput = checkInput(amount, "Общая стоимость");
+  let sumDebt = amountInput - contributionInput;
   let dateToDay = new Date();
   let dateEnd = new Date(date);
   let numberOfMonth = monthDiff(dateToDay, dateEnd);   
-  let percInMonth = percent / 12 / 100; 
+  let percInMonth = percentInput / 12 / 100; 
   let payInMonth = sumDebt * ( percInMonth + percInMonth / (Math.pow( 1 + percInMonth, numberOfMonth )-1 ));
- // console.log(payInMonth);
   let totalAmount = payInMonth * numberOfMonth;
- // console.log(totalAmount);
   return totalAmount;
 }
 
@@ -58,22 +55,8 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-  let greeting;
-  /*switch (name) {
-    case "":
-    case "null":
-    case "undefined":
-      greeting = `Привет, мир! Меня зовут Аноним.`;
-    default:
-      greeting = `Привет, мир! Меня зовут ${name}.`;
-  } не работает (*/
-
   if (name === "" || name === "null" || name === "undefined") {
-    greeting = `Привет, мир! Меня зовут Аноним.`;
-  } else {
-    greeting = `Привет, мир! Меня зовут ${name}.`;
-  }
-  console.log(greeting);
-  return greeting;
+    name = "Аноним";
+  } 
+  return  `Привет, мир! Меня зовут ${name}.`;
 }
