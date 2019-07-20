ef5c8c3664448e276b5b735e05d71f9d7ef33662
Irina Butuzova <butuzovairina18@gmail.com>
2019-07-20 13:06:51 +0300

    second commit array-proto-hocs

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,40 +1,61 @@
 function compareArrays(arr1, arr2) {
-   return JSON.stringify(arr1) === JSON.stringify(arr2);
-}
-const sum = (a, b) => a + b;
-//console.log(fn(5, 7));
-
-
-let results = [];
-function memoize(fn, limit){
-   
-   let arrArguments = Array.from(arguments).join(', ');
-   if (!results.includes(arrArguments)) {
-     if (results.length < limit) {
-      results.push({arrArguments, fn});
-      console.log(results);
-      return fn;
-     } else {
-      results.shift();
-      results.push({arrArguments, fn});
-     }  
+   if (arr1.length === arr2.length) {
+     return !arr1.some((item, i) => item != arr2[i]); 
+   } else {
+     return false;
    }
 }
 
-//const mSum = memoize(fn, 10) ; 
-console.log (memoize(sum(3, 4 ), 10)); // 7
-console.log (memoize(sum(5, 4 ), 10)); // 7
-console.log (memoize(sum(3, 14 ), 10)); // 7
-console.log (memoize(sum(3, 4 ), 10)); // 7
-console.log (memoize(sum(5, 4 ), 10)); // 7
-console.log (memoize(sum(3, 14 ), 10)); // 7
-console.log (memoize(sum(3, 4 ), 10)); // 7
-console.log (memoize(sum(5, 4 ), 10)); // 7
-console.log (memoize(sum(3, 14 ), 10)); // 7
-console.log (memoize(sum(3, 4 ), 10)); // 7
-console.log (memoize(sum(5, 4 ), 10)); // 7
-console.log (memoize(sum(3, 14 ), 10)); // 7
+const sum = (a, b) => (a + b);
+const mSum = memoize(sum, 10); 
+console.log(sum(5, 8));
 
+function memoize(fn, limit){
+   let results = [];
+   console.log(fn, limit);
+     return function () {
+    let arrArguments = Array.from(arguments).join(', ');
+   console.log(arrArguments); 
+   
+    res = fn();
+  console.log(results);
+   console.log(res);
+     results.push({arrArguments, res});
+   console.log('2  ' + results);
+     }
+   /*   
+   if (!results.includes(arrArguments)) {
+
+     if (results.length < limit) {
+      
+      console.log("lll" + results);
+        } else {
+      results.shift();
+      results.push({arrArguments, fn});
+     }
+   } */
+   
+}
+
+
+
+
+console.log (mSum(3, 4)); // 7
+console.log (mSum(53, 4)); // 7
+console.log (mSum(34, 4)); // 7
+console.log (mSum(3, 4)); // 7
+/*console.log (memoize(sum(5, 4 ), 10)); // 7
+console.log (memoize(sum(3, 14 ), 10)); // 7
+console.log (memoize(sum(3, 4 ), 10)); // 7
+console.log (memoize(sum(5, 4 ), 10)); // 7
+console.log (memoize(sum(3, 14 ), 10)); // 7
+console.log (memoize(sum(3, 4 ), 10)); // 7
+console.log (memoize(sum(5, 4 ), 10)); // 7
+console.log (memoize(sum(3, 14 ), 10)); // 7
+console.log (memoize(sum(3, 4 ), 10)); // 7
+console.log (memoize(sum(5, 4 ), 10)); // 7
+console.log (memoize(sum(3, 14 ), 10)); // 7
+*/
 
 console.log(compareArrays([8, 9], [6])); // false, разные значения
 console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])); // false, разные значения
