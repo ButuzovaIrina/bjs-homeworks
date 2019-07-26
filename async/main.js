a40c284d8f4d30438ef7f8398b5f476c76f1f65f
Irina Butuzova <butuzovairina18@gmail.com>
2019-07-26 13:41:56 +0300

    changed the mistakes

--- async/main.js
+++ async/main.js
@@ -1,38 +1,39 @@
-function goodMorning() {
-    console.log("Доброе утро, Вася!");
-}
-
-function sweetDream() {
-    console.log("Спокойной ночи, Вася!");
-}
-
 function setAlarm(time, callback) {
-  let now = new Date();
-  let nowHours, nowMinutes;
-  if (now.getHours() < 10) {
-    nowHours = "0" + now.getHours();
-  } else {
-    nowHours = now.getHours();
-  }
-  if (now.getMinutes() < 10) {
-    nowMinutes = "0" + now.getMinutes();
-  } else {
-    nowMinutes = now.getMinutes();
-  }
-  let nowString = nowHours + ":" + nowMinutes; 
-  if (time == nowString) {
+ 
+  return setInterval((() => { 
+    let now = new Date();
+    let nowHours, nowMinutes;
+    if (now.getHours() < 10) {
+      nowHours = "0" + now.getHours();
+    } else {
+      nowHours = now.getHours();
+    }
+    if (now.getMinutes() < 10) {
+      nowMinutes = "0" + now.getMinutes();
+    } else {
+      nowMinutes = now.getMinutes();
+    }
+    let nowString = nowHours + ":" + nowMinutes; 
+    if (time == nowString) {
     return callback();
-  }
+    }
+  }), 1000);
+ 
 }
-
-
 
 function setDailyRhythm(wakeUpTime, bedTime) {
   
-   setAlarm(wakeUpTime, goodMorning);
-   setAlarm(bedTime, sweetDream)
+  function goodMorning() {
+    console.log("Доброе утро, Вася!");
+  }
+
+  function sweetDream() {
+    console.log("Спокойной ночи, Вася!");
+  }
+  setAlarm(wakeUpTime, goodMorning);
+  setAlarm(bedTime, sweetDream);
 }
 
-setInterval(setDailyRhythm, 1000, "07:39", "23:11");
+setDailyRhythm("07:26", "13:38");
 
 
